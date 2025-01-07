bfcc75fbafd43bbddd0425260e5d19d8f9e927ab
Anmol chalise <anmolc650@gmail.com>
2025-01-07 12:03:15 +0545

    rescale and transform

---
+++ Table_extraction/model/preproces.py
@@ -0,0 +1,47 @@
+import torch
+from torchvision import transforms
+from load import *
+import torch
+from torchvision import transforms
+
+class MaxResize(object):
+    def __init__(self, max_size=800):
+        self.max_size = max_size
+
+    def __call__(self, image):
+        width, height = image.size
+        current_max_size = max(width, height)
+        scale = self.max_size / current_max_size
+        resized_image = image.resize((int(round(scale*width)), int(round(scale*height))))
+
+        return resized_image
+
+detection_transform = transforms.Compose([
+    MaxResize(800),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+pixel_values = detection_transform(image).unsqueeze(0)
+pixel_values = pixel_values.to(device)
+
+
+with torch.no_grad():
+  outputs = model(pixel_values)
+
+id2label = model.config.id2label
+id2label[len(model.config.id2label)] = "no object"
+
+
+  # for output bounding box post-processing
+def box_cxcywh_to_xyxy(x):
+    x_c, y_c, w, h = x.unbind(-1)
+    b = [(x_c - 0.5 * w), (y_c - 0.5 * h), (x_c + 0.5 * w), (y_c + 0.5 * h)]
+    return torch.stack(b, dim=1)
+
+
+def rescale_bboxes(out_bbox, size):
+    img_w, img_h = size
+    b = box_cxcywh_to_xyxy(out_bbox)
+    b = b * torch.tensor([img_w, img_h, img_w, img_h], dtype=torch.float32)
+    return b
+

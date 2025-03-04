7dff0a7baa7c9b0ad46aaec3ceac70e18fa3b58d
Anmol <anmolc650@gmail.com>
2025-03-04 14:07:42 +0545

    initalizing valid scripts

--- YoloTrOCR/Table_extraction/__init__.py
+++ YoloTrOCR/Table_extraction/__init__.py
@@ -1,3 +1,3 @@
-from cell_coordinates import *
-from crop_table import *
-from Table_extraction.model.preprocess import *
+from .cell_coordinates import *
+from .crop_table import *
+from .preprocess import *
